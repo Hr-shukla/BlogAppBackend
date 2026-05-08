00b1cfeaaa9703d54b08ae1dcc88f97d19f2a810
HARSH RAJ SHUKLA <hrofficial6458@gmail.com>
2026-05-08 23:37:40 +0530

    first

---
+++ models/commentModel.js
@@ -0,0 +1,21 @@
+//importb mongoose
+
+const mongoose = require("mongoose");
+
+//route handler 
+const commentSchema = new mongoose.Schema({
+    post:{
+        type: mongoose.Schema.Types.ObjectIf,
+        ref:"Post",
+    },
+    user:{
+        type:String,
+        required:true,
+    },
+    body:{
+        type:String,
+        required:true,
+    }
+});
+//export
+module.exports= mongoose.model("Comments",commentSchema);
---
+++ routes/blog.js
@@ -0,0 +1,16 @@
+const express= require("express");
+const router = express.Router();
+
+//import controller
+
+
+
+
+
+//mapping
+
+
+
+
+
+//export
